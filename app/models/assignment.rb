f2ea1f25ed99f956295eb1e04c399682a012529e
lokeshh <lokeshhsharma@gmail.com>
2016-02-06 19:02:50 +0000

    SEtup up model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,9 @@
 class Assignment < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_attached_file :doc,
+    :storage => :google_drive,
+    :google_drive_credentials => "#{Rails.root}/config/google_drive.yml",
+    :google_drive_options => {
+      :public_folder_id => '0B4VcRFzXj-Leak4tdGg2czdvbTQ'
+    }
 end
