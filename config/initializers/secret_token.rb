b8a887a633eda1e7fe3353542cd8d891540bfdd6
lokeshh <lokeshhsharma@gmail.com>
2016-02-06 18:27:33 +0000

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colab::Application.config.secret_token = '324905a6f018aa3b9891b393b154cf0ee4ee2fd80ae292baac7397cf8e1f1f57c48ce0faddb90437ebe4bc3333d588ad23b8c26f9f100182d1fcbc84aeb34c88'
